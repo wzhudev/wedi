5c62977ccfe6b1b5dd9698ae6aac34e4405c5132
Wendell <wendellhu95@gmail.com>
2020-04-03 11:39:59 +0800

    :white_check_mark: add behavior subject test (#3)
    * :construction_worker: fix CI

--- test/di-rx.test.tsx
+++ test/di-rx.test.tsx
@@ -1,14 +1,16 @@
 import { act, render } from '@testing-library/react';
 import React, { Component } from 'react';
-import { interval } from 'rxjs';
+import { BehaviorSubject, interval, Subject } from 'rxjs';
 import { scan, startWith } from 'rxjs/operators';
 
 import {
+  IDisposable,
   Provide,
   Provider,
   useCollection,
   useDependency,
-  useDependencyValue
+  useDependencyValue,
+  useUpdateBinder
 } from '../src';
 
 describe('di-rx', () => {
@@ -43,6 +45,52 @@ describe('di-rx', () => {
     expect(container.firstChild!.textContent).toBe('3');
   });
 
+  it('should use default value in BehaviourSubject', async () => {
+    class CounterService implements IDisposable {
+      public counter$: BehaviorSubject<number>;
+      private number: number;
+      private loop?: number;
+
+      constructor() {
+        this.number = 5;
+        this.counter$ = new BehaviorSubject(this.number);
+        this.loop = (setInterval(() => {
+          this.number += 1;
+          this.counter$.next(this.number);
+        }, 1000) as any) as number;
+      }
+
+      dispose(): void {
+        clearTimeout(this.loop!);
+      }
+    }
+
+    function App() {
+      const collection = useCollection([CounterService]);
+
+      return (
+        <Provider collection={collection}>
+          <Child />
+        </Provider>
+      );
+    }
+
+    function Child() {
+      const counterService = useDependency(CounterService);
+      const count = useDependencyValue(counterService.counter$);
+
+      return <div>{count}</div>;
+    }
+
+    const { container } = render(<App />);
+    expect(container.firstChild!.textContent).toBe('5');
+
+    await act(
+      () => new Promise<undefined>((res) => setTimeout(() => res(), 3200))
+    );
+    expect(container.firstChild!.textContent).toBe('8');
+  });
+
   it('should not trigger unnecesary re-render when handled correctly', async () => {
     let childRenderCount = 0;
 
@@ -72,7 +120,6 @@ describe('di-rx', () => {
 
     function Child(props: { count?: number }) {
       childRenderCount += 1;
-      console.log(props.count);
       return <div>{props.count}</div>;
     }
 
@@ -85,5 +132,51 @@ describe('di-rx', () => {
     );
     expect(container.firstChild!.textContent).toBe('3');
     expect(childRenderCount).toBe(4);
+  });
+
+  it('should update whenever `useUpdateBinder` emits', async () => {
+    class CounterService implements IDisposable {
+      public number = 0;
+      public updater$ = new Subject<void>();
+
+      private loop?: number;
+
+      constructor() {
+        this.loop = (setInterval(() => {
+          this.number += 1;
+          this.updater$.next();
+        }, 1000) as any) as number;
+      }
+
+      dispose(): void {
+        clearTimeout(this.loop!);
+      }
+    }
+
+    function App() {
+      const collection = useCollection([CounterService]);
+
+      return (
+        <Provider collection={collection}>
+          <Child />
+        </Provider>
+      );
+    }
+
+    function Child() {
+      const counterService = useDependency(CounterService);
+
+      useUpdateBinder(counterService.updater$);
+
+      return <div>{counterService.number}</div>;
+    }
+
+    const { container } = render(<App />);
+    expect(container.firstChild!.textContent).toBe('0');
+
+    await act(
+      () => new Promise<undefined>((res) => setTimeout(() => res(), 3200))
+    );
+    expect(container.firstChild!.textContent).toBe('3');
   });
 });
